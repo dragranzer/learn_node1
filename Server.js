6eb440877c895846537d3e6e2ecbd81dcc287266
rizadwiandhika <rizadwiandhika@gmail.com>
2021-09-01 16:58:28 +0700

    BREAKING CHANGE: image now accessible

--- Server.js
+++ Server.js
@@ -1,31 +1,10 @@
-let http = require('http');
-let fs = require('fs');
-let express = require('express');
-let app = express();
+let express = require('express')
+let app = express()
 
-const PORT=8080; 
-app.use('/img', express.static('img')); 
-app.use(express.static('public'));  
-// app.listen(PORT, () => {
-//     console.log(`Running server on PORT ${PORT}...`); 
-// })
-fs.readFile('public/home.html', function (err, html) {
+const PORT = 8080
 
-    if (err) throw err;
-    
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
-});
-// fs.readFile('./home.html', function (err, html) {
+app.use(express.static('public'))
 
-//     if (err) throw err;
-    
-//     http.createServer(function(request, response) {  
-//         response.writeHeader(200, {"Content-Type": "text/html"});  
-//         response.write(html);  
-//         response.end();  
-//     }).listen(PORT);
-// });
+app.listen(PORT, () => {
+  console.log(`Running server on PORT ${PORT}...`)
+})
